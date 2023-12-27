90d446c21bb60d8f2fa9cf2180ba4b425f68feb6
VanTrang <workdvt811212@gmail.com>
2023-12-27 22:20:49 +0700

    FEA: api/user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,26 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+
+
+router.get('/api/user', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'user1',
+                age: '30',
+            },
+            {
+                name: 'user2',
+                age: '31',
+            }, {
+                name: 'user3',
+                age: '32',
+            }
+        ]
+    })
+})
+
 module.exports = router;
